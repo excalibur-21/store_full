b35e91080a9b7884268f07e4999dec0b7697c5f6
excalibur-21 <svistun.kirill2017@yandex.ru>
2023-08-18 10:08:40 +0300

    initial commit

---
+++ products/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from products.models import Product, ProductCategory
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', queryset=ProductCategory.objects.all())
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
